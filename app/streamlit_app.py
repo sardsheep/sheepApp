870d0dae05d91c7ec3269a59cc5896e97c7ea9a1
sardsheep <sardsheep@gmail.com>
2025-11-04 12:23:45 +0100

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -144,6 +144,8 @@ sql_current = build_sql(include_type=(type_clause != ""), include_behaviour=True
 # --- 7) Run query and show table ---
 try:
     with InfluxDBClient3(host=URL, token=TOKEN, org=ORG, database=DB) as client:
+        st.code(sql_current, language="sql")
+
         result = client.query(sql_current)
 
     if isinstance(result, pd.DataFrame):
