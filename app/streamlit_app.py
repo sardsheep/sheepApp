8c3bcc9dbef37b2357358339ab5f49f165eb916f
sardsheep <sardsheep@gmail.com>
2025-11-17 12:00:29 +0100

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -172,11 +172,17 @@ sql_current = build_sql(include_type=(type_clause != ""), include_behaviour=True
 
 # --- 7) Run query and show table ---
 try:
+
+
+
     with InfluxDBClient3(host=URL, token=TOKEN, org=ORG, database=DB) as client:
-        st.code(sql_current, language="sql")
+    if SHOW_DEBUG:
+        st.code(sql_current, language="sql")  # only visible when SHOW_DEBUG = True
 
-        result = client.query(sql_current)
+    result = client.query(sql_current)
 
+
+    
     if isinstance(result, pd.DataFrame):
         df = result
     elif isinstance(result, pa.Table):
